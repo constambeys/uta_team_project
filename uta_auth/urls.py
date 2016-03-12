5b65abc7086fee6f1be86cd0599d08943daabfa1
Nickolas Zoulis <nickolaszoulis@gmail.com>
2016-03-12 18:48:25 +0000

    some student_homepage styling and removed unecessary url login

--- uta_auth/urls.py
+++ uta_auth/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from uta_auth import views
 
 urlpatterns = patterns('',
-        url(r'^user_login/$', views.user_login, name='user_login'),
         url(r'^register/(?P<user_type>.*)/$', views.register_user, name='register_user'),
         url(r'^register/$', views.register, name='register'),
         url(r'^logout/$', views.user_logout, name='logout'),
