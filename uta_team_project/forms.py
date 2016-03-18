bca4343e9fb2a2b6d03e99b7b2683c34d098ab85
constambeys <timo0190@gmail.com>
2016-03-18 23:20:06 +0000

    update team create

--- uta_team_project/forms.py
+++ uta_team_project/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from uta_models.models import *
 
 
@@ -9,6 +10,17 @@ class AssignmentForm(forms.ModelForm):
 
 
 class GroupForm(forms.ModelForm):
+    def __init__(self, students, limit, data=None):
+        super(forms.ModelForm, self).__init__(data)
+        self.fields['students'].queryset = students
+        self.limit = limit
+
+    def clean_students(self):
+        students = self.cleaned_data['students']
+        if students.count() > self.limit:
+            raise ValidationError("Maximum number allowed is " + str(self.limit))
+        return students
+
     class Meta:
         model = Group
         fields = ('name', 'students')
@@ -22,4 +34,3 @@ class RequirementsForm(forms.ModelForm):
 
 class CourseAssignmentForm(forms.Form):
     course_assignment = forms.ModelChoiceField(queryset=Assignment.objects.all().order_by('name'))
-
